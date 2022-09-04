c43258b36d3db465e005e59aabecf11a24fa8d47
johndoe1409 <mikhail.murzaev@gmail.com>
2022-09-04 20:04:22 +0300

    sixth_homework

---
+++ tests/test_admin_product.py
@@ -0,0 +1,34 @@
+from selector.admin_product import AddProduct
+from selector.admin_product import Login
+from selector.admin_product import DeleteProduct
+
+
+def test_add_product(browser, url):
+    auth = Login(browser, url)
+    auth.open(url)
+    auth.login()
+    auth.password()
+    auth.login_button()
+    auth.catalog()
+    auth.products()
+    add = AddProduct(browser, url)
+    add.add_new()
+    add.name_product()
+    add.meta_teg()
+    add.link_data()
+    add.model()
+    add.save()
+
+
+def test_delete_product(browser, url):
+    auth = Login(browser, url)
+    auth.open(url)
+    auth.login()
+    auth.password()
+    auth.login_button()
+    auth.catalog()
+    auth.products()
+    delete = DeleteProduct(browser, url)
+    delete.accept_product()
+    delete.delete()
+    delete.assert_confirmation
